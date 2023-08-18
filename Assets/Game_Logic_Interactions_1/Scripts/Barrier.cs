6ff48b923abcc9963b8f7cd396405dc0199b08b8
TimTheGr8 <tjanssen4@gmail.com>
2023-08-18 15:18:06 -0500

    Fixed the collider issue on the barrier not disabling.

--- Assets/Game_Logic_Interactions_1/Scripts/Barrier.cs
+++ Assets/Game_Logic_Interactions_1/Scripts/Barrier.cs
@@ -11,12 +11,16 @@ public class Barrier : MonoBehaviour
 
     private int _currentHealth;
     private MeshRenderer _renderer;
+    private Collider _collider;
 
     private void Start()
     {
         _renderer = GetComponent<MeshRenderer>();
         if (_renderer == null)
             Debug.LogError("There is no MeshRenderer on the barrier.");
+        _collider = GetComponent<Collider>();
+        if (_collider == null)
+            Debug.LogError("There is no collider on this object.");
         _currentHealth = _maxHealth;
     }
 
@@ -26,6 +30,7 @@ public class Barrier : MonoBehaviour
         if(_currentHealth <= 0)
         {
             StartCoroutine(RechargeBarrier(_rechargeTime));
+            _collider.enabled = false;
             _renderer.enabled = false;
         }
     }
@@ -34,6 +39,7 @@ public class Barrier : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         _renderer.enabled = true;
+        _collider.enabled = true;
         _currentHealth = _maxHealth;
     }
 }
